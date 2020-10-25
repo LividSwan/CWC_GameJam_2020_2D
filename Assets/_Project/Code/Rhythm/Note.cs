4b83a99ad32761fe429f5095d4d313fba57a4caa
Sean Edward Gordon-Davis <gitned42@gmail.com>
2020-10-26 00:11:36 +0200

    DanceScene initial working state

--- Assets/_Project/Code/Rhythm/Note.cs
+++ Assets/_Project/Code/Rhythm/Note.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static GameJam.Rhythm.MusicConductor;
 
 namespace GameJam.Rhythm
 {
@@ -14,20 +15,16 @@ namespace GameJam.Rhythm
         private string _key1;
         private string _key2;
 
-        private float _removeAtX = -20;
+        private float _finalXPosition;
 
         public bool canBePressed;
-        public List<KeyCode> keyCodeList;
 
         private float _beatTempo;
-        public float tempo;
+        private float _tempo;
 
         private void Awake()
         {
             _inputController = InputController.Instance;
-
-            _key1 = Char.ToLowerInvariant(keyCodeList[0].ToString()[0]) + keyCodeList[0].ToString().Substring(1);
-            _key2 = Char.ToLowerInvariant(keyCodeList[1].ToString()[0]) + keyCodeList[1].ToString().Substring(1);
         }
 
         private void OnEnable()
@@ -40,7 +37,7 @@ namespace GameJam.Rhythm
         // Start is called before the first frame update
         void Start()
         {
-            _beatTempo = tempo / 60f;
+            _beatTempo = _tempo / 60f;
         }
 
         // Update is called once per frame
@@ -53,11 +50,46 @@ namespace GameJam.Rhythm
             //    );
 
             transform.position += new Vector3(-_beatTempo * Time.deltaTime, 0f);
+            //transform.position += Vector3.Lerp(transform.position, new Vector3(-_beatTempo * Time.deltaTime, 0f), -_beatTempo * Time.deltaTime);
 
-            if (transform.position.x < _removeAtX)
+            if (transform.position.x < _finalXPosition)
             {
                 NotePool.Instance.ReturnToPool(this);
             }
+        }
+
+        public void SpawnNote(ArrowDirection arrowdirection, Vector3 spawnPosition, float tempo, float finalXPosition)
+        {
+            switch (arrowdirection)
+            {
+                case ArrowDirection.UP:
+                    _key1 = "w";
+                    _key2 = "upArrow";
+                    transform.Rotate(0f, 0f, 0f);
+                    break;
+                case ArrowDirection.DOWN:
+                    _key1 = "s";
+                    _key2 = "downArrow";
+                    transform.Rotate(0f, 0f, 180f);
+                    break;
+                case ArrowDirection.LEFT:
+                    _key1 = "a";
+                    _key2 = "leftArrow";
+                    transform.Rotate(0f, 0f, 90f);
+                    break;
+                case ArrowDirection.RIGHT:
+                    _key1 = "d";
+                    _key2 = "rightArrow";
+                    transform.Rotate(0f, 0f, -90f);
+                    break;
+                default:
+                    break;
+            }
+
+            transform.position = spawnPosition;
+            _tempo = tempo;
+            _finalXPosition = finalXPosition;
+            this.gameObject.SetActive(true);
         }
 
         private void OnTriggerEnter2D(Collider2D other)
@@ -85,7 +117,7 @@ namespace GameJam.Rhythm
                 if (keyPressed == _key1 || keyPressed == _key2)
                 {
                     //Debug.Log($"KeyCodes = {_key1} {_key2} keyPressed = {keyPressed}");
-                    gameObject.SetActive(false);
+                    NotePool.Instance.ReturnToPool(this);
                 }
             }
         }
